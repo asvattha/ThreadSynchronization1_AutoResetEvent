77c4ce69900d246ba232f6047c0c0f87f9187895
singhd1 <singhd1@sfasu.edu>
2021-11-17 14:17:51 -0600

    Add project files.

---
+++ ThreadSynchronization1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadSynchronization1
+{
+    class Program
+    {
+
+        private static object lockHandle = new object();
+        public static int result = 0;
+
+        public static EventWaitHandle readyForResult = new AutoResetEvent(false);
+        public static EventWaitHandle setResult = new AutoResetEvent(false);
+
+
+
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(DoWork);
+            t.Start();
+
+            for(int i = 0; i < 100; i++)
+            {
+                readyForResult.Set();
+
+                setResult.WaitOne();
+
+                lock (lockHandle)
+                {
+                    Console.WriteLine(result);
+                }
+                Thread.Sleep(10);
+            }
+        }
+
+        public static void DoWork()
+        {
+            while (true)
+            {
+                int i = result;
+                Thread.Sleep(1);
+
+                readyForResult.WaitOne();
+                lock (lockHandle)
+                {
+                    result = i + 1;
+                }
+                setResult.Set();
+                
+                
+            }
+        }
+    }
+}
